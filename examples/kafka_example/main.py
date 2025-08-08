5e016c6a62671f95494c8be432a20953b4585d3b
jack.burridge <jack.burridge@mail.com>
2025-08-08 17:36:34 +0100

    feat: initial implementation of aiokafka-acgi
    - initial implementation handling single messages
    
    BREAKING CHANGE:

---
+++ examples/kafka_example/main.py
@@ -0,0 +1,38 @@
+import os
+from dataclasses import dataclass
+from typing import AsyncGenerator
+from typing import Iterable
+from typing import Tuple
+
+import aiokafka_acgi
+from asyncfast import AsyncFast
+from pydantic import BaseModel
+
+
+class Payload(BaseModel):
+    id: str
+
+
+@dataclass
+class Message:
+    address: str
+    headers: Iterable[Tuple[bytes, bytes]]
+    payload: bytes
+
+
+app = AsyncFast()
+
+
+@app.channel("input_topic")
+async def input_topic_handler(payload: Payload) -> AsyncGenerator[Message, None]:
+    print("received message", payload)
+    yield Message(
+        address="output_topic",
+        headers=[(b"id", str(payload.id).encode())],
+        payload=f'{{"state":"processed","id":{payload.id}}}'.encode(),
+    )
+
+
+if __name__ == "__main__":
+    bootstrap_servers = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+    aiokafka_acgi.run(app, "input_topic", bootstrap_servers=bootstrap_servers)
