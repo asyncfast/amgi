ae87c5a3d366aa9f60ec34ddf4e7ae44d3d0fd61
jack.burridge <jack.burridge@mail.com>
2025-08-10 17:04:30 +0100

    feat: introduce cli, allowing for generation of AsyncAPI

---
+++ packages/asyncfast/tests/test_asyncapi.py
@@ -0,0 +1,95 @@
+from typing import Annotated
+
+from asyncfast import AsyncFast
+from asyncfast import Header
+from pydantic import BaseModel
+
+
+def test_asyncapi_header() -> None:
+    app = AsyncFast()
+
+    @app.channel("hello")
+    async def on_hello(request_id: Annotated[int, Header()]) -> None:
+        pass
+
+    assert app.asyncapi() == {
+        "asyncapi": "3.0.0",
+        "channels": {
+            "OnHello": {
+                "address": "hello",
+                "messages": {
+                    "OnHelloMessage": {"$ref": "#/components/messages/OnHelloMessage"}
+                },
+            }
+        },
+        "components": {
+            "messages": {
+                "OnHelloMessage": {
+                    "headers": {"$ref": "#/components/schemas/OnHelloHeaders"}
+                }
+            },
+            "schemas": {
+                "OnHelloHeaders": {
+                    "properties": {
+                        "request-id": {"title": "Request-Id", "type": "integer"}
+                    },
+                    "title": "OnHelloHeaders",
+                    "type": "object",
+                }
+            },
+        },
+        "info": {"title": "AsyncFast", "version": "0.1.0"},
+        "operations": {
+            "receiveOnHello": {
+                "action": "receive",
+                "channel": {"$ref": "#/channels/OnHello"},
+            }
+        },
+    }
+
+
+def test_asyncapi_payload() -> None:
+    app = AsyncFast()
+
+    class Payload(BaseModel):
+        id: int
+        name: str
+
+    @app.channel("hello")
+    async def on_hello(payload: Payload) -> None:
+        pass
+
+    assert app.asyncapi() == {
+        "asyncapi": "3.0.0",
+        "channels": {
+            "OnHello": {
+                "address": "hello",
+                "messages": {
+                    "OnHelloMessage": {"$ref": "#/components/messages/OnHelloMessage"}
+                },
+            }
+        },
+        "components": {
+            "messages": {
+                "OnHelloMessage": {"payload": {"$ref": "#/components/schemas/Payload"}}
+            },
+            "schemas": {
+                "Payload": {
+                    "properties": {
+                        "id": {"title": "Id", "type": "integer"},
+                        "name": {"title": "Name", "type": "string"},
+                    },
+                    "required": ["id", "name"],
+                    "title": "Payload",
+                    "type": "object",
+                }
+            },
+        },
+        "info": {"title": "AsyncFast", "version": "0.1.0"},
+        "operations": {
+            "receiveOnHello": {
+                "action": "receive",
+                "channel": {"$ref": "#/channels/OnHello"},
+            }
+        },
+    }
