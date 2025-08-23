eb5170e8c96c0c2fa537b70177f903c0c90a52c2
jack.burridge <jack.burridge@mail.com>
2025-08-23 17:11:33 +0100

    docs: add initial documentation for Message class

---
+++ packages/asyncfast/docs/examples/send_message.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Annotated
+from typing import AsyncGenerator
+
+from asyncfast import AsyncFast
+from asyncfast import Header
+from asyncfast import Message
+
+app = AsyncFast()
+
+
+@dataclass
+class OutputMessage(Message, address="output_channel"):
+    id: Annotated[int, Header()]
+    payload: str
+
+
+@app.channel("input_channel")
+async def input_channel_handler() -> AsyncGenerator[OutputMessage, None]:
+    yield OutputMessage(id=1, payload="Hello")
