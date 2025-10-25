39b91da0d37212d9a259e47bb7370f74e56d49c8
jack.burridge <jack.burridge@mail.com>
2025-10-25 21:55:53 +0100

    test: move MockApp to new package test-utils so that it may be reused during tests

--- packages/amgi-aiokafka/tests_amgi_aiokafka/test_kafka_message_integration.py
+++ packages/amgi-aiokafka/tests_amgi_aiokafka/test_kafka_message_integration.py
@@ -1,42 +1,13 @@
 import asyncio
-from asyncio import Event
-from asyncio import Queue
 from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
 from uuid import uuid4
 
 import pytest
 from aiokafka import AIOKafkaConsumer
 from aiokafka import AIOKafkaProducer
 from amgi_aiokafka import Server
-from amgi_types import AMGIReceiveCallable
-from amgi_types import AMGISendCallable
-from amgi_types import Scope
+from test_utils import MockApp
 from testcontainers.kafka import KafkaContainer
-
-
-class MockApp:
-    def __init__(self) -> None:
-        self._call_queue = Queue[
-            tuple[Scope, AMGIReceiveCallable, AMGISendCallable, Event]
-        ]()
-
-    @asynccontextmanager
-    async def call(
-        self,
-    ) -> AsyncGenerator[tuple[Scope, AMGIReceiveCallable, AMGISendCallable], None]:
-        scope, receive, send, return_event = await self._call_queue.get()
-        try:
-            yield scope, receive, send
-        finally:
-            return_event.set()
-
-    async def __call__(
-        self, scope: Scope, receive: AMGIReceiveCallable, send: AMGISendCallable
-    ) -> None:
-        return_event = Event()
-        self._call_queue.put_nowait((scope, receive, send, return_event))
-        await return_event.wait()
 
 
 @pytest.fixture(scope="session")
