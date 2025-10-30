bca22cbbe76348feff69f2a893b6a6e9c1ad4f3a
jack.burridge <jack.burridge@mail.com>
2025-10-30 13:38:56 +0000

    feat: add a redis amgi server

---
+++ packages/amgi-redis/tests_amgi_redis/test_redis_message_integration.py
@@ -0,0 +1,114 @@
+import asyncio
+from collections.abc import AsyncGenerator
+from typing import Any
+from uuid import uuid4
+
+import pytest
+from amgi_redis import Server
+from redis.asyncio.client import PubSub
+from test_utils import MockApp
+from testcontainers.redis import AsyncRedisContainer
+
+
+@pytest.fixture(scope="session")
+async def redis_container() -> AsyncGenerator[AsyncRedisContainer, None]:
+    with AsyncRedisContainer(image="redis:8.2.2") as redis_container:
+        yield redis_container
+
+
+@pytest.fixture
+def channel() -> str:
+    return f"receive-{uuid4()}"
+
+
+@pytest.fixture
+async def app(
+    redis_container: AsyncRedisContainer, channel: str
+) -> AsyncGenerator[MockApp, None]:
+
+    app = MockApp()
+
+    host = redis_container.get_container_host_ip()
+    port = redis_container.get_exposed_port(redis_container.port)
+    server = Server(app, channel, url=f"redis://{host}:{port}")
+    loop = asyncio.get_running_loop()
+    serve_task = loop.create_task(server.serve())
+    async with app.call() as (scope, receive, send):
+        assert scope == {
+            "amgi": {"spec_version": "1.0", "version": "1.0"},
+            "type": "lifespan",
+            "state": {},
+        }
+        lifespan_startup = await receive()
+        assert lifespan_startup == {"type": "lifespan.startup"}
+        await send({"type": "lifespan.startup.complete"})
+        yield app
+        server.stop()
+        lifespan_shutdown = await receive()
+        assert lifespan_shutdown == {"type": "lifespan.shutdown"}
+        await send({"type": "lifespan.shutdown.complete"})
+
+    await serve_task
+
+
+async def test_message(
+    app: MockApp, channel: str, redis_container: AsyncRedisContainer
+) -> None:
+    client = await redis_container.get_async_client()
+
+    await client.publish(channel, "value")
+
+    async with app.call() as (scope, receive, send):
+        assert scope == {
+            "address": channel,
+            "amgi": {"spec_version": "1.0", "version": "1.0"},
+            "type": "message",
+            "state": {},
+        }
+
+        message_receive = await receive()
+        assert message_receive == {
+            "headers": [],
+            "id": "",
+            "more_messages": False,
+            "payload": b"value",
+            "type": "message.receive",
+        }
+
+
+async def _get_message(pubsub: PubSub) -> Any:
+    while True:
+        message = await pubsub.get_message(ignore_subscribe_messages=True, timeout=None)
+        if message is not None and message["type"] == "message":
+            return message
+
+
+async def test_message_send(
+    app: MockApp, channel: str, redis_container: AsyncRedisContainer
+) -> None:
+    client = await redis_container.get_async_client()
+
+    await client.publish(channel, "value")
+    send_channel = f"send-{uuid4()}"
+
+    async with client.pubsub() as pubsub:
+        await pubsub.subscribe(send_channel)
+
+        async with app.call() as (scope, receive, send):
+            await send(
+                {
+                    "type": "message.send",
+                    "address": send_channel,
+                    "headers": [],
+                    "payload": b"test",
+                }
+            )
+
+        message = await _get_message(pubsub)
+
+        assert message == {
+            "type": "message",
+            "pattern": None,
+            "channel": send_channel.encode(),
+            "data": b"test",
+        }
