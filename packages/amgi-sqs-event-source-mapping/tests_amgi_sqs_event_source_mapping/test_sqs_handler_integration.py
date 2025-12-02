4d5a4448920ee36f7a45cfef0dd9e3a23f081928
jack.burridge <jack.burridge@mail.com>
2025-12-02 18:12:21 +0000

    perf(amgi-sqs-event-source-mapping): use caching, and batching to improve sqs performance

--- packages/amgi-sqs-event-source-mapping/tests_amgi_sqs_event_source_mapping/test_sqs_handler_integration.py
+++ packages/amgi-sqs-event-source-mapping/tests_amgi_sqs_event_source_mapping/test_sqs_handler_integration.py
@@ -3,6 +3,7 @@ from collections.abc import AsyncGenerator
 from uuid import uuid4
 
 import pytest
+from amgi_sqs_event_source_mapping import SqsBatchFailureError
 from amgi_sqs_event_source_mapping import SqsHandler
 from test_utils import MockApp
 from testcontainers.localstack import LocalStackContainer
@@ -65,32 +66,12 @@ async def test_sqs_handler_record_send(
         )
     )
     async with app.call() as (scope, receive, send):
-        assert scope == {
-            "type": "message",
-            "amgi": {"version": "1.0", "spec_version": "1.0"},
-            "address": "my-queue",
-            "state": {},
-        }
-
-        assert await receive() == {
-            "type": "message.receive",
-            "id": "059f36b4-87a3-44ab-83d2-661975830a7d",
-            "headers": [(b"myAttribute", b"myValue")],
-            "payload": b"Test message.",
-            "more_messages": False,
-        }
         await send(
             {
                 "type": "message.send",
                 "address": send_queue_name,
                 "headers": [(b"test", b"test")],
                 "payload": b"test",
-            }
-        )
-        await send(
-            {
-                "type": "message.ack",
-                "id": "059f36b4-87a3-44ab-83d2-661975830a7d",
             }
         )
 
@@ -105,5 +86,68 @@ async def test_sqs_handler_record_send(
             "test": {"StringValue": "test", "DataType": "StringValue"}
         }
 
-    batch_item_failures = await call_task
-    assert batch_item_failures == {"batchItemFailures": []}
+    await call_task
+
+
+async def test_sqs_handler_record_send_invalid_message(
+    localstack_container: LocalStackContainer,
+) -> None:
+    app = MockApp()
+    sqs_handler = SqsHandler(
+        app,
+        region_name=localstack_container.region_name,
+        endpoint_url=localstack_container.get_url(),
+        aws_access_key_id="testcontainers-localstack",
+        aws_secret_access_key="testcontainers-localstack",
+        lifespan=False,
+    )
+    sqs_client = localstack_container.get_client("sqs")
+
+    send_queue_name = f"send-{uuid4()}"
+    sqs_client.create_queue(
+        QueueName=send_queue_name, Attributes={"MaximumMessageSize": "1024"}
+    )
+
+    call_task = asyncio.get_running_loop().create_task(
+        sqs_handler._call(
+            {
+                "Records": [
+                    {
+                        "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
+                        "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
+                        "body": "Test message.",
+                        "attributes": {
+                            "ApproximateReceiveCount": "1",
+                            "SentTimestamp": "1545082649183",
+                            "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+                            "ApproximateFirstReceiveTimestamp": "1545082649185",
+                        },
+                        "messageAttributes": {
+                            "myAttribute": {
+                                "stringValue": "myValue",
+                                "stringListValues": [],
+                                "binaryListValues": [],
+                                "dataType": "String",
+                            }
+                        },
+                        "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+                        "eventSource": "aws:sqs",
+                        "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+                        "awsRegion": "us-east-2",
+                    }
+                ]
+            },
+        )
+    )
+    async with app.call() as (scope, receive, send):
+        with pytest.raises(SqsBatchFailureError):
+            await send(
+                {
+                    "type": "message.send",
+                    "address": send_queue_name,
+                    "headers": [],
+                    "payload": b"a" * 1025,
+                }
+            )
+
+    await call_task
