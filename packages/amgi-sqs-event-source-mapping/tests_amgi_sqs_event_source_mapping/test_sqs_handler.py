2f2de7f24ed0d0b04f458f6c8da067290bc3e0c6
jack.burridge <jack.burridge@mail.com>
2025-12-04 17:21:35 +0000

    test(amgi-sqs-event-source-mapping): add test for when invalid arn is in lambda event

--- packages/amgi-sqs-event-source-mapping/tests_amgi_sqs_event_source_mapping/test_sqs_handler.py
+++ packages/amgi-sqs-event-source-mapping/tests_amgi_sqs_event_source_mapping/test_sqs_handler.py
@@ -3,6 +3,7 @@ import base64
 from collections.abc import AsyncGenerator
 from collections.abc import Generator
 from queue import Queue
+from unittest.mock import AsyncMock
 from unittest.mock import Mock
 from unittest.mock import patch
 from uuid import uuid4
@@ -457,3 +458,40 @@ def test_lifespan_and_shutdown() -> None:
 
     exception = queue.get()
     assert exception is None
+
+
+def test_sqs_handler_app_not_called_if_invalid_arn() -> None:
+    mock_app = AsyncMock()
+    sqs_handler = SqsHandler(mock_app, lifespan=False)
+    sqs_handler(
+        {
+            "Records": [
+                {
+                    "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
+                    "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
+                    "body": "Test message.",
+                    "attributes": {
+                        "ApproximateReceiveCount": "1",
+                        "SentTimestamp": "1545082649183",
+                        "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+                        "ApproximateFirstReceiveTimestamp": "1545082649185",
+                    },
+                    "messageAttributes": {
+                        "myAttribute": {
+                            "stringValue": "myValue",
+                            "stringListValues": [],
+                            "binaryListValues": [],
+                            "dataType": "String",
+                        }
+                    },
+                    "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+                    "eventSource": "aws:sqs",
+                    "eventSourceARN": "invalid-arn:aws:sqs:us-east-2:123456789012:my-queue",
+                    "awsRegion": "us-east-2",
+                }
+            ]
+        },
+        Mock(),
+    )
+
+    mock_app.assert_not_awaited()
